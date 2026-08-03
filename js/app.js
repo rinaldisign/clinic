5e26952d8251e17f58a55c99fc490a870ddb5b30
rinaldisign <rinaldisign@gmail.com>
2026-08-03 13:52:27 +0900

    app.js

--- js/app.js
+++ js/app.js
@@ -31,7 +31,7 @@ const scenes = [
     dot: { x: 33.7, y: 77.0 },
     yawOffset: 0,
     hotspots: [
-      { pitch: -25, yaw: 350, target: "scene2", text: "患者室" },
+      { pitch: -22, yaw: 354, target: "scene2", text: "患者室" },
     ],
   },
   {
